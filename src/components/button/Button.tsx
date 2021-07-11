fd21ade6505afbdc3f9e1587a3d008f61b6f0ba3
xiongshuang <xsmails@163.com>
2021-07-11 14:19:22 +0800

    Button 新增loading

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,13 @@
 import './style.styl';
 import React, { FC } from 'react';
+import LoadingSvg from '@/assets/iconSvg/loading.svg';
 import { Props } from './types';
 
 const Button: FC<Props> = ({
     type = 'default',
     size = 'medium',
     disabled = false,
+    loading = false,
     width = 80,
     children,
     click = () => {}
@@ -24,6 +26,7 @@ const Button: FC<Props> = ({
         }}
     >
         <section className="d-btn-content">
+            {loading && <span className="d-btn-loading"><LoadingSvg /></span>}
             <span className="d-btn-text">{children}</span>
         </section>
     </div>
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -4,6 +4,7 @@ export type Props = {
     type?: string
     size?: string
     disabled?: boolean
+    loading?: boolean
     width?: number
     click?: Fn
 }
